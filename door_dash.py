98fb883aa46e8c71aaa5581e8253459f3a9a4e29
Harsimran <dhillo42@myumanitoba.ca>
2023-04-26 13:41:24 -0500

    Doordash: userInput done

--- door_dash.py
+++ door_dash.py
@@ -1,6 +1,7 @@
-# importing required libraries
+#importing required libraries
 import ctypes
 import time
+import sys
 import pandas as pd
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
@@ -16,7 +17,11 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import re
 
-import sys
+#get user input
+user_address= input("Please enter where you would like the food to be delivered?")
+requested_restaurant= input("Please enter from which restaurant you would like to order food?")
+requested_food_item= input("Please enter what food item you would like to order?")
+
 
 
 #specify the complete path to the Google Chrome binary.
@@ -24,18 +29,17 @@ options = Options()
 options= uc.ChromeOptions()
 options.binary_location = "/Applications/Google Chrome 2.app/Contents/MacOS/Google Chrome"
 #chrome_driver_binary = "/usr/local/bin/chromedriver"
-#options.add_argument('--headless')
 driver = uc.Chrome(use_subprocess=True, options=options)
 
 driver.maximize_window()
 
 #Take the user input to obtain the URL of the website to be scraped, and web scrape the page.
-val = "https://www.doordash.com/en-CA/"
+VAL= "https://www.doordash.com/en-CA/"
 wait = WebDriverWait(driver, 10) #10 seconds before timing out
-driver.get(val) #loads the web page in the selected browser session
+driver.get(VAL) #loads the web page in the selected browser session
 get_url = driver.current_url #get the url of the current page
-wait.until(EC.url_to_be(val)) #Calls the method provided with the driver as an argument until the return value does not evaluate to False. EC.url_to_be checks the current url to become the expected URL "val"
-if get_url == val:  #ensure that the correct url has been opened
+wait.until(EC.url_to_be(VAL)) #Calls the method provided with the driver as an argument until the return value does not evaluate to False. EC.url_to_be checks the current url to become the expected URL "val"
+if get_url == VAL:  #ensure that the correct url has been opened
     page_source = driver.page_source # get the entire HTML source code of a webpage as a string
 
 
@@ -44,7 +48,7 @@ driver.switch_to.window(driver.window_handles[0])
 time.sleep(3)
 
 element= driver.find_element(By.CSS_SELECTOR, "input[class='Input__InputContent-sc-1o75rg4-3 froUFe']")
-element.send_keys("")
+element.send_keys(user_address)
 time.sleep(3)
 element.send_keys(Keys.RETURN)
 
@@ -52,39 +56,38 @@ driver.implicitly_wait(130)
 time.sleep(3)
 
 element= driver.find_element(By.CSS_SELECTOR, "input[class='Input__InputContent-sc-1o75rg4-3 froUFe']")
-element.send_keys("Korean Garden")
+element.send_keys(requested_restaurant)
 time.sleep(3)
 element.send_keys(Keys.RETURN)
 driver.implicitly_wait(120)
 
-restaurant_container= driver.find_element(By.XPATH,"//div[@data-anchor-id='StoreLayoutListContainer']")
+restaurant_container=driver.find_element(By.XPATH,"//div[@data-anchor-id='StoreLayoutListContainer']")
 
-def checkIfClosed():
-        try:
-            exitButton = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Close the \"this store is closed\" modal']")
-            if exitButton:
-                 exitButton.click()
-        except NoSuchElementException:
-            pass
+def check_if_closed():
+    """
+    Docstring
+    """
+    try:
+        exit_button = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Close the \"this store is closed\" modal']")
+        if exit_button.is_displayed():
+            exit_button.click()
+    except NoSuchElementException:
+        pass
 
 try:
-    restaurant= restaurant_container.find_element(By.XPATH, ".//span[@data-telemetry-id='store.name' and contains(text(), 'Korean Garden')]") #stores all shown restaurant names
+    restaurant= restaurant_container.find_element(By.XPATH, f".//span[@data-telemetry-id='store.name' and contains(text(), '{requested_restaurant}')]") #stores all shown restaurant names
     restaurant.click()
     driver.implicitly_wait(120)
     time.sleep(5)
     #exit out of the currently closed notif
-    checkIfClosed()
+    #check_if_closed()
 
-    driver.implicitly_wait(120)
-    #going to placed into its own function 
-   
+    #going to placed into its own function
     menu_container=  driver.find_element(By.XPATH, "//div[@data-anchor-id='MenuItem']")
-    menu = menu_container.find_element(By.XPATH,"//button[contains(@aria-label, 'House Roll')]")
+    menu = menu_container.find_element(By.XPATH, f"//button[contains(@aria-label, '{requested_food_item}')]")
     price= re.search(r'\$(\d+\.\d+)', menu.text).group(1)
     print(price)
 
 except NoSuchElementException:
     print("Restaurant not found.")
-
-
 #driver.close()
