dd748ea44f5beefe0c85e15a67880312c33ce3a9
Harsimran <dhillo42@myumanitoba.ca>
2023-04-25 22:37:29 -0500

    add removed

--- sel_dp2.py
+++ sel_dp2.py
@@ -13,6 +13,8 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC #provides a set of predefined conditions that can be used to wait for specific events to occur on a webpage.
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import re
 
 import sys
 
@@ -22,6 +24,7 @@ options = Options()
 options= uc.ChromeOptions()
 options.binary_location = "/Applications/Google Chrome 2.app/Contents/MacOS/Google Chrome"
 #chrome_driver_binary = "/usr/local/bin/chromedriver"
+#options.add_argument('--headless')
 driver = uc.Chrome(use_subprocess=True, options=options)
 
 driver.maximize_window()
@@ -41,11 +44,11 @@ driver.switch_to.window(driver.window_handles[0])
 time.sleep(3)
 
 element= driver.find_element(By.CSS_SELECTOR, "input[class='Input__InputContent-sc-1o75rg4-3 froUFe']")
-element.send_keys("82 Huber street")
+element.send_keys("")
 time.sleep(3)
 element.send_keys(Keys.RETURN)
 
-driver.implicitly_wait(120)
+driver.implicitly_wait(130)
 time.sleep(3)
 
 element= driver.find_element(By.CSS_SELECTOR, "input[class='Input__InputContent-sc-1o75rg4-3 froUFe']")
@@ -56,17 +59,32 @@ driver.implicitly_wait(120)
 
 restaurant_container= driver.find_element(By.XPATH,"//div[@data-anchor-id='StoreLayoutListContainer']")
 
-restaurant_names = restaurant_container.find_elements(By.XPATH, ".//span[@data-telemetry-id='store.name']") #stores all shown restaurant names
-  
-#check if the restaurant is on the food service site
-for restaurant in restaurant_names:
-    if restaurant.text == "Korean Garden":
-    #click on the restaurant to go its page
-        restaurant.click()
-        driver.implicitly_wait(120)
-        time.sleep(3)
-    else:
-        print("Restaurant not found in Doordash.")
+def checkIfClosed():
+        try:
+            exitButton = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Close the \"this store is closed\" modal']")
+            if exitButton:
+                 exitButton.click()
+        except NoSuchElementException:
+            pass
+
+try:
+    restaurant= restaurant_container.find_element(By.XPATH, ".//span[@data-telemetry-id='store.name' and contains(text(), 'Korean Garden')]") #stores all shown restaurant names
+    restaurant.click()
+    driver.implicitly_wait(120)
+    time.sleep(5)
+    #exit out of the currently closed notif
+    checkIfClosed()
+
+    driver.implicitly_wait(120)
+    #going to placed into its own function 
+   
+    menu_container=  WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@data-anchor-id='MenuItem']")))
+    menu = menu_container.find_element(By.XPATH,".//button[contains(@aria-label, 'House Roll')]")
+    price= re.search(r'\$(\d+\.\d+)', menu.text).group(1)
+    print(price)
+
+except NoSuchElementException:
+    print("Restaurant not found.")
 
 
 #driver.close()
